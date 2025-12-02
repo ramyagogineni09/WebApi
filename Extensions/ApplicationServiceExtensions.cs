426fa0e2b6a8fae1544ca363d94513377438974a
ramyagogineni09 <ramyagogineni08@gmail.com>
2025-12-02 10:31:16 +0530

    Intial project setup with migrations

---
+++ Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,40 @@
+﻿//using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Webapi.Data;
+using Webapi.Models;
+
+namespace Webapi.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services,
+            IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection")
+              ?? throw new InvalidOperationException("Connection string 'DefaultConnection' notfound");
+            services.AddDbContext<DataContext>(options=>options.UseSqlServer(connectionString));
+
+            //configure sessions options
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+
+            });
+
+            //services.AddIdentity<ApplicationUser, IdentityRole>()
+            //    .AddEntityFrameworkStores<DataContext>()
+            //    .AddDefaultTokenProviders();
+
+
+
+            services.AddControllers();
+            services.AddCors();
+
+            return services;
+
+        }
+    }
+}
