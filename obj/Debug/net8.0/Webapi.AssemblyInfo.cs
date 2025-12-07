4c6ab24ebd5a1d9fde99fb4f7089cc64a3678fdd
ramyagogineni09 <ramyagogineni08@gmail.com>
2025-12-07 08:18:48 +0530

    Added create appointment features

--- obj/Debug/net8.0/Webapi.AssemblyInfo.cs
+++ obj/Debug/net8.0/Webapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d0a6b29a7edcf4764c8fc15d0dd750c7d019468")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8210c23b11b688002369fea6fd3211469e224491")]
 [assembly: System.Reflection.AssemblyProductAttribute("Webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
